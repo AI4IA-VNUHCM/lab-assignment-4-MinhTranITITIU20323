8c7723a9f8f1d0492e976367466099c5ed868a20
MinhTranIU2410 <ITITIU20323@student.hcmiu.edu.vn>
2021-10-25 20:01:55 +0700

    Exercise 1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -14,10 +14,23 @@ ______________________________________
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+int binomialCoefficients(int i, int j) {
+	if (i == 0 || j == i)
+   		return 1;
+   	return binomialCoefficients(i - 1, j - 1) + binomialCoefficients(i - 1, j);
+
+}
 
 void Ex1(int n){
 	//Your codes here
-    
+	int i,j;
+    for(i=0;i<n;i++){
+		for(j=0;j<=i;j++){
+			int temp=binomialCoefficients(i,j);
+			printf(" %d",temp);
+		}
+		printf("\n");
+	}
 }
 
 int main(int argc, char *argv[]) {
